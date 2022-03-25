fe33fb5e3de97c5b35dce029fbd8d93bfa034dbd
cosminbujac <bujac.cosmin@gmail.com>
2022-03-25 10:50:46 +0200

    Add MyLocation and fix ActionBar to not overlay the fragments

---
+++ app/src/main/java/com/ubb/licenta/MapUtil.kt
@@ -0,0 +1,13 @@
+package com.ubb.licenta
+
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+
+object MapUtil {
+    fun setCameraPosition(location: LatLng): CameraPosition {
+        return CameraPosition.Builder()
+            .target(location)
+            .zoom(18f)
+            .build()
+    }
+}
--- app/src/main/java/com/ubb/licenta/MapsFragment.kt
+++ app/src/main/java/com/ubb/licenta/MapsFragment.kt
@@ -1,47 +1,65 @@
 package com.ubb.licenta
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.ubb.licenta.MapUtil.setCameraPosition
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(),OnMapReadyCallback {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private lateinit var map:GoogleMap
+    private lateinit var fusedLocationProviderClinet : FusedLocationProviderClient
+    private lateinit var mapView : View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        fusedLocationProviderClinet = LocationServices.getFusedLocationProviderClient(requireActivity())
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapView = mapFragment?.view!!
+        mapFragment?.getMapAsync(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        val myLocation = fusedLocationProviderClinet.lastLocation
+        myLocation.addOnCompleteListener{
+            map.animateCamera((
+                    CameraUpdateFactory.newCameraPosition(
+                        setCameraPosition(LatLng(it.result.latitude,it.result.longitude)
+                        )
+                    )),1000,null)
+        }
+
+        map.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isRotateGesturesEnabled = false
+            isTiltGesturesEnabled = false
+
+        }
     }
 }
